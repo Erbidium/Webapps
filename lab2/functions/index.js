b71bd7f2c6c8c8ec2a9cbce41210d4d273c17bde
Oleksandr <OlexandrTanasienko@gmail.com>
2021-10-24 18:52:07 +0300

    Lab 2
    Fix dependencies, fix firebase config, add firebase secrets, remove unnecessary lines of code
    
    
    Fix gitignore
    
    
    Ignore config with variables
    
    
    Fix form style
    
    
    Add limited number of requests for ip
    
    
    Fix form style, remove unnecessary elements, fix rate limit
    
    
    Change background colors

---
+++ lab2/functions/index.js
@@ -0,0 +1,72 @@
+const sanitizeHtml = require("sanitize-html");
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+
+const mail = functions.config().form.mailgunadress;
+
+const transporter = nodemailer.createTransport({
+
+  host: "smtp.mailgun.org",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: mail,
+    pass: functions.config().form.mailgunpass,
+  },
+});
+
+const rateLimit = {
+  callLimitForOneIp: 5,
+  timeInSeconds: 20,
+  ipCache: new Map(),
+};
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+  const reqIp = req.headers["fastly-client-ip"];
+  let ipUser = {};
+  const now = new Date();
+  if (rateLimit.ipCache.get(reqIp)===undefined) {
+    rateLimit.ipCache.set(reqIp, {reqCount: 1, time: now});
+  } else {
+    ipUser = rateLimit.ipCache.get(reqIp);
+    ipUser.reqCount+=1;
+    rateLimit.ipCache.set(reqIp, ipUser);
+    console.log("req number " + ipUser.reqCount);
+    console.log("current time" + (now - ipUser.time));
+    if ((ipUser.reqCount > rateLimit.callLimitForOneIp)||
+        (now - ipUser.time <= rateLimit.timeInSeconds * 1000)) {
+      return res.status(429)
+          .json({code: "429", error: "Too many requests!"});
+    }
+  }
+  ipUser = rateLimit.ipCache.get(reqIp);
+  ipUser.time = new Date();
+  rateLimit.ipCache.set(reqIp, ipUser);
+
+  res.send("Hello from Firebase!");
+
+  if (!Object.keys(req.body ? req.body : {}).length) {
+    return res.status(400).json({code: 400, error: "No message!"});
+  }
+  const lines = Object.entries(req.body)
+      .map(([key, val]) => `<p><b>${key}: </b>${val}</p>`)
+      .join("\n");
+  const html = `<p><b>Message from contact form:</b>${lines}</p>`;
+  const htmlSan = sanitizeHtml(html);
+
+  const mailOptions={
+    from: `Contact form <${mail}>`,
+    to: functions.config().form.mailadressto,
+    subject: "Message contact form", // Subject line,
+    date: (new Date()).toUTCString(),
+    html: htmlSan, // html body
+  };
+
+  transporter.sendMail(mailOptions, (error)=>{
+    if (error) {
+      console.error("Error sending mail", error.message);
+      return res.status(500).json({code: "500", error: error.message});
+    }
+    return res.status(200).json({data: "ok"});
+  });
+});
---
+++ lab2/src/routes/index.svelte
@@ -0,0 +1,109 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import {Jumper} from 'svelte-loading-spinners';
+
+	 let form={
+		 reset:()=>{}
+	 }
+	let errorText = "";
+	let showSpinner = false;
+	let statusMessage = false;
+	let errorMessage = false;
+	let formBtnDisable = false;
+	function resetFormStatus(){
+		statusMessage = false;
+		errorMessage = false;
+		formBtnDisable = false;
+	}
+	let contactFormHandler = async (e) =>{
+		formBtnDisable = true;
+		showSpinner = true;
+
+		const referrerValue = document.referrer;
+
+		let formData={
+			'User name': form.elements.userName.value,
+			Email: form.elements.userEmail.value,
+			Message: form.elements.userMessage.value,
+			referrer: referrerValue
+		};
+		try {
+			await fetch("/api/sendMail",{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(formData)
+			}).then((res) => {
+				if(res.status >= 200 && res.status < 300){
+					return res;
+				}
+				else {
+					throw res;
+				}
+			})
+			statusMessage = true;
+			form.reset();
+			showSpinner = false;
+		}catch(e){
+			if(e.status >= 500){
+				errorText = 'Email doesn`t exist';
+			}
+			else if(e.status === 400)
+			{
+				errorText = "Empty email message!";
+			}
+			else if(e.status === 429)
+			{
+				errorText = "You sent mail too many times";
+			}
+			statusMessage = false;
+			errorMessage = true;
+			console.log(e);
+			showSpinner = false;
+			formBtnDisable = false;
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		Please, fill form
+	</h1>
+	<form class="contact-form" bind:this={form} on:submit|preventDefault={contactFormHandler}>
+		<input class="contact-form-input" type="text" name="userName" placeholder="Name" required>
+		<input class="contact-form-input" type="email" name="userEmail" placeholder="Email" required>
+		<textarea class="contact-form-message"
+				  name="userMessage"
+				  cols="30"
+				  rows="10"
+				  placeholder="Message text"
+				  required
+		/>
+		{#if statusMessage}
+			<p class="status-text success">
+				Message sent!
+				<button class="button class-btn" on:click={resetFormStatus}> &times; </button>
+			</p>
+		{:else if errorMessage}
+			<p class="status-text error">
+				{errorText}
+				<button class="button class-btn" on:click={resetFormStatus}> &times; </button>
+			</p>
+		{/if}
+
+		<button class="button contact-form-button" type="submit" disabled={formBtnDisable}>
+			{#if showSpinner}
+				<Jumper size="60" color="#FF3E00" unit="px" duration="1s"></Jumper>
+			{/if}
+			Submit
+		</button>
+	</form>
+</section>
