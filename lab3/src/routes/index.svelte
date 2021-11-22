da9236d20c59bf212bd85bb09ca5b51b8953718c
Oleksandr <OlexandrTanasienko@gmail.com>
2021-11-22 02:50:37 +0200

    add buttons for creating notes

--- lab3/src/routes/index.svelte
+++ lab3/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
 
+  import {onMount} from "svelte";
   async function fetchGraphQL(operationsDoc, operationName, variables) {
     const result = await fetch(
             "https://lab3todo.herokuapp.com/v1/graphql",
@@ -98,7 +99,19 @@
     const targetElement = event.target;
     startExecuteDeleteNote(targetElement.id);
   }
-  startFetchMyQuery();
+  let checkIcon;
+  let xIcon;
+
+  function typeNote()
+  {
+    console.log(xIcon);
+    console.log("Rejected!");
+  }
+  function createNote()
+  {
+    console.log(checkIcon);
+    console.log("Note is created!");
+  }
 </script>
 
 <svelte:head>
@@ -111,6 +124,20 @@
     <p>...waiting</p>
   {:then data}
     <p>Totally notes: {data.data.notes.length}</p>
+    <form>
+      <textarea id="note-text" placeholder="Write note..." maxlength="96">
+
+      </textarea>
+      <svg bind:this={checkIcon} on:click={createNote} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+      </svg>
+      <svg bind:this={xIcon} on:click={typeNote} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+      </svg>
+    </form>
+    <button class="createNote">Create note</button>
     <button class="btnDeleteAll" on:click={startExecuteDeleteAllMutation} >Delete all</button>
     <ul>
       {#each data.data.notes as {author, date,  text, id}}
