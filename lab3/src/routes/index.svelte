25467f90908cd7205e86522982977e9238f049ff
Oleksandr <OlexandrTanasienko@gmail.com>
2021-12-06 00:07:08 +0200

    Fix hiding of note to fill

--- lab3/src/routes/index.svelte
+++ lab3/src/routes/index.svelte
@@ -180,18 +180,18 @@
   let isDisabled="visible";
   let errorOccured =false;
   let error;
+  let displayValue = "none";
 
   function typeNote()
   {
-    if(isDisabled==="hidden")
+    if(displayValue!=="none")
     {
-      inputNote.visible=false;
-      isDisabled="visible";
+      displayValue="none";
     }
     else {
-      inputNote.visible=true;
-      isDisabled="visible";
+      displayValue="block";
     }
+    console.log("IsDisabled: "+isDisabled);
     console.log(xIcon);
     console.log("Rejected!");
 
@@ -221,15 +221,15 @@
     <p style="color: red">{error}</p>
   {:else}
     <p>Totally notes: {notes.length}</p>
-    <form>
+    <form style="--display-value: {displayValue}">
       <textarea id="note-text" bind:this={inputNote} placeholder="Write note..." maxlength="96">
 
       </textarea>
-      <svg bind:this={checkIcon} on:click={createNote} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+      <svg id="check-icon" bind:this={checkIcon} on:click={createNote} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
         <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
       </svg>
-      <svg bind:this={xIcon} on:click={typeNote} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+      <svg id="x-icon" bind:this={xIcon} on:click={typeNote} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
       </svg>
@@ -254,3 +254,9 @@
     </ul>
   {/if}
 </div>
+
+<style>
+  form {
+    display: var(--display-value);
+  }
+</style>
