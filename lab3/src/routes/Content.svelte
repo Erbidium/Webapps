3cfcc695b7abf88026fb9c835c30ef8041e5f4d2
Oleksandr <OlexandrTanasienko@gmail.com>
2021-12-16 19:23:18 +0200

    Create modal

---
+++ lab3/src/routes/Content.svelte
@@ -0,0 +1,14 @@
+<!-- Content.svelte -->
+<script>
+    import { getContext } from 'svelte';
+    import Surprise from './Surprise.svelte';
+
+    const { open } = getContext('simple-modal');
+
+
+    export function showSurprise() {
+        open(Surprise, { message: "Error occured" });
+    }
+</script>
+
+<p><button on:click={showSurprise}>Show me a surprise!</button></p>
