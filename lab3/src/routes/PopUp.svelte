143136689395bfcc131b6730a4c7ec76bdc544b9
Oleksandr <OlexandrTanasienko@gmail.com>
2021-12-16 20:47:17 +0200

    Create custom PopUp component

---
+++ lab3/src/routes/PopUp.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let popUpMessage;
+</script>
+
+<p>
+    {popUpMessage}
+</p>
+
+<style>
+    p{
+        background-color: red;
+        border-radius: 15px;
+        position: fixed;
+        right:30px;
+        bottom:30px;
+        padding: 10px 15px;
+    }
+</style>
