24d3619dc1e71acc3dc5ab0fa21529ae2fdc8ea1
Oleksandr <OlexandrTanasienko@gmail.com>
2021-12-18 18:11:06 +0200

    Write code for getting and displaying todos from hasura

---
+++ lab3/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  plugins: ['svelte3', 'sonarjs', 'prettier'],
+  extends: ['eslint:recommended', 'prettier', 'plugin:sonarjs/recommended'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
